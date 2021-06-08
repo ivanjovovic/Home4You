8c3ee012520e47d7e56a4c88b675e6bded2874bf
Ivan <ivanjovovic98@gmail.com>
2021-06-08 13:17:48 +0200

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,27 @@
+var images = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg','img6.jpg'];
+
+var i =0;
+
+function slideShow() {
+
+    document.getElementById("image").src=images[i];
+
+
+
+    if(i<images.length-1){
+
+        i++;
+
+    }
+
+    else {
+
+        i=0;
+
+    }
+
+    setTimeout("slideShow()" , 2000);
+
+}
+
+window.onload = slideShow;
